9ea8ef01a8b4f45193d05838f7b6415e9c99322c
rootoring <magomedovzalimhan49@gmail.com>
2024-01-21 16:56:53 +0300

    first

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1'
+    }
+  },
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt'
+  ],
+  css: ["bootstrap/dist/css/bootstrap.min.css"]
+})
